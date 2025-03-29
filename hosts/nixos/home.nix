3bb23bdd1b4305fe41d9b625125f277df4b3fdca
Sean <sean.zhong98@gmail.com>
2025-03-30 00:07:56 +0100

    move p10k conf

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -24,14 +24,16 @@
     };
     history.size = 10000;
 
-    initExtra = ''
-      [[ ! -f ${./p10k.zsh} ]] || source ${./p10k.zsh}
-    '';
     plugins = [
       {
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k-config;
+        file = "p10k.zsh";
       }
     ];
 
