9b603217db15b000450b9008366ba50d2e2c7c7c
Sean Zhong <sean.zhong@scila.se>
2025-03-31 18:03:29 +0200

    Add rdp

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -57,6 +57,18 @@
     variant = "";
   };
 
+  # RDP
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+  services.xrdp.openFirewall = true;
+
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
