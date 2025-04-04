74fb29869a5fab738f1fe5fd42f589e5d2f7a72c
Sean <sean.zhong98@gmail.com>
2025-04-05 00:13:47 +0200

    use lua file for wezterm conf

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -76,25 +76,7 @@
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = true;
-    extraConfig = ''
-    -- Pull in the wezterm API
-    local wezterm = require 'wezterm'
-
-    -- This will hold the configuration.
-    local config = wezterm.config_builder()
-
-    -- This is where you actually apply your config choices
-
-    -- For example, changing the color scheme:
-    config.color_scheme = 'Rapture'
-    config.font_size = 10.0
-    config.font = wezterm.font 'JetBrainsMono Nerd Font'
-    config.enable_wayland = false
-    config.window_background_opacity = 0.7
-
-    -- and finally, return the configuration to wezterm
-    return config
-    '';
+    extraConfig = builtins.readFile ./dotfiles/wezterm/wezterm.lua;
   };
 
   programs.tmux = {
