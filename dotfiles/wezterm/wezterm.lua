beee9279f81ed15184287fee85db9141a7159f6e
Sean Zhong <sean.zhong@scila.se>
2025-04-05 13:58:35 +0200

    Change wezterm conifg

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -38,7 +38,7 @@ config.keys = {
     },
     {
         mods = "LEADER",
-        key = "|",
+        key = "\",
         action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
     },
     {
@@ -101,7 +101,7 @@ end
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
-config.tab_and_split_indices_are_zero_based = true
+config.tab_and_split_indices_are_zero_based = false
 
 -- tmux status
 wezterm.on("update-right-status", function(window, _)
@@ -114,12 +114,12 @@ wezterm.on("update-right-status", function(window, _)
         SOLID_LEFT_ARROW = utf8.char(0xe0b2)
     end
 
-    if window:active_tab():tab_id() ~= 0 then
-        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+    if window:active_tab():tab_id() ~= 1 then
+        ARROW_FOREGROUND = { Foreground = { Color = "#000000" } }
     end -- arrow color based on if tab is first pane
 
     window:set_left_status(wezterm.format {
-        { Background = { Color = "#b7bdf8" } },
+        { Background = { Color = "#3d3846" } },
         { Text = prefix },
         ARROW_FOREGROUND,
         { Text = SOLID_LEFT_ARROW }
