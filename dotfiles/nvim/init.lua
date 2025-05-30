d20f578a88d05889d083e46aeaa7d20bf666d43f
Sean Zhong <sean.zhong@scila.se>
2025-05-30 13:18:21 +0200

    Changed nvim colorscheme to machiato

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -4,6 +4,9 @@ require("plugins/treesitter")
 require("plugins/lualine")
 require("plugins/lsp-config")
 require("plugins/completions")
+require("catppuccin").setup({
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+})
 
 vim.cmd.colorscheme "catppuccin"
 
