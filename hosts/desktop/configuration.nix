34b0e1fb29487bc0fb26ea747f784210054a4ad9
Sean Zhong <sean.zhong98@gmail.com>
2025-09-07 13:23:28 +0200

    Correct hostname on desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -20,7 +20,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "desktop"; # Define your hostname.
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Enable networking
