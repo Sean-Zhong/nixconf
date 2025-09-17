a1c305b72324454b4e82e3bff4451f99a4f97d4d
Sean Zhong <sean.zhong98@gmail.com>
2025-09-17 21:07:03 +0200

    Feature: Switch to home manager module, add some policies

--- modules/home-manager/zenbrowser.nix
+++ modules/home-manager/zenbrowser.nix
@@ -1,10 +1,32 @@
 { pkgs, inputs, system, ... }:
 {
-  home.packages = [
-    (
-      inputs.zen-browser.packages."${system}".default.override {
-        nativeMessagingHosts = [pkgs.firefoxpwa];
-      }
-    )
+  imports = [
+    inputs.zen-browser.homeModules.twilight
   ];
+
+  programs.zen-browser = {
+    enable = true;
+
+    nativeMessagingHosts = [pkgs.firefoxpwa];
+
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+  };
 }
+
