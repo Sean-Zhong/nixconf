a855885b13bbb33b3ac91550b4acc9990ffd93d2
Sean Zhong <sean.zhong@scila.se>
2025-10-13 13:57:34 +0200

    Fix: Make homemanager follow nixpackages stable

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
