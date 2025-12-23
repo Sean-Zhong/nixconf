1950e37faf7d6b94d46849e5b3d39332abab494b
Sean Zhong <sean.zhong@scila.se>
2025-12-23 10:48:50 +0100

    Feature: Add java 25 binary and session var

--- modules/home-manager/java.nix
+++ modules/home-manager/java.nix
@@ -3,13 +3,15 @@
 {
     home.file."jdks/temurin11".source = pkgs.temurin-bin-11;
     home.file."jdks/temurin17".source = pkgs.temurin-bin-17;
-    home.file."jdks/temurin21".source = pkgs.temurin-bin;
+    home.file."jdks/temurin21".source = pkgs.temurin-bin-21;
+    home.file."jdks/temurin25".source = pkgs.temurin-bin-25;
     home.file."jdks/jetbrains".source = pkgs.jetbrains.jdk;
 
     home.sessionVariables = {
         JAVA_11_HOME = "$HOME/jdks/temurin11";
         JAVA_17_HOME = "$HOME/jdks/temurin17";
         JAVA_21_HOME = "$HOME/jdks/temurin21";
+        JAVA_25_HOME = "$HOME/jdks/temurin25";
         JETBRAINS_CLIENT_JDK = "$HOME/jdks/jetbrains";
     };
 
