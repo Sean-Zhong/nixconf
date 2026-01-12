a1f2310b7d19d39ce3e42247ed8612029d0e009a
Sean Zhong <sean.zhong@scila.se>
2026-01-12 12:52:58 +0100

    Fix: Fis ibus not configured notificaton on startup

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -36,5 +36,23 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-    programs.gdk-pixbuf.modulePackages = [ pkgs.librsvg ];
+  programs.gdk-pixbuf.modulePackages = [ pkgs.librsvg ];
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [
+      libpinyin
+    ];
+  };
+
+  environment.sessionVariables = {
+    GTK_IM_MODULE = "ibus";
+    QT_IM_MODULE = "ibus";
+    XMODIFIERS = "@im=ibus";
+    GLFW_IM_MODULE = "ibus"; 
+  };
+
+  programs.dconf.enable = true;
 }
+
