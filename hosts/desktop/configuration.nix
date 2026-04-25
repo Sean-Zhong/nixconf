218f5d9cd132ad526ea96d6bd61f7120fcc7b03d
Sean Zhong <sean.zhong98@gmail.com>
2026-04-25 21:18:15 +0200

    Fix: Pin desktop kernel to 6.19

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -16,7 +16,7 @@
     ];
 
   # Bootloader.
-  #boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_19;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
