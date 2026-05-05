cf47220281dce9fefd9f4f5885a3cdf7da103d38
Sean Zhong <sean.zhong98@gmail.com>
2026-05-06 00:21:06 +0200

    Feature: Change color scheme in wezterm

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -4,10 +4,7 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'Rapture'
+config.color_scheme = 'Catppuccin Macchiato'
 config.font_size = 16.0
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.enable_wayland = true
