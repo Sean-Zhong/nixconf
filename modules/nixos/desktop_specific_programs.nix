98356c93c6df0df0c11174573453c9246b0e6df1
Sean Zhong <sean.zhong98@gmail.com>
2026-05-21 18:46:39 +0200

    Feature: Add ananicy to prioritize games pids

--- modules/nixos/desktop_specific_programs.nix
+++ modules/nixos/desktop_specific_programs.nix
@@ -18,7 +18,16 @@
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
 
-  programs.gamescope.enable = true;
+  programs.gamescope = {
+    enable = true;
+    #capSysNice = true;
+  };
+
+  services.ananicy = {
+    enable = true;
+    package = pkgs.ananicy-cpp;
+    rulesProvider = pkgs.ananicy-rules-cachyos;
+  };
 
   services.ratbagd.enable = true;
 }
