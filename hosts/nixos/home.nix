a6da2d868fbb637e5e18e8af3fb7ae0f7c097624
Sean Zhong <sean.zhong@scila.se>
2026-05-26 14:46:15 +0200

    Special: remove vpn toggle

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -12,7 +12,6 @@
     ../../modules/home-manager/zenbrowser.nix
     ../../modules/home-manager/kubernetes.nix
     ../../modules/home-manager/ssh.nix
-    ../../modules/home-manager/vpn-toggle.nix
     ../../modules/home-manager/development.nix
     ../../modules/home-manager/secrets.nix
     ];
