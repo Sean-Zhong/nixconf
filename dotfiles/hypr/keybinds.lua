dade26e2c8094482db357083d70f7fb2d982572b
Sean Zhong <sean.zhong@scila.se>
2026-06-10 16:57:49 +0200

    Switch to walker for cliphist

--- dotfiles/hypr/keybinds.lua
+++ dotfiles/hypr/keybinds.lua
@@ -20,7 +20,7 @@ hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(browser))
 hl.bind(mainMod .. " + I", hl.dsp.exec_cmd(ide))
 hl.bind(mainMod .. " + escape", hl.dsp.exec_cmd("hyprlock"))
 hl.bind(mainMod .. " + W", hl.dsp.exec_cmd("systemctl --user restart waybar"))
-hl.bind("SUPER + V", hl.dsp.exec_cmd("cliphist list | wofi --dmenu | cliphist decode | wl-copy"))
+hl.bind("SUPER + V", hl.dsp.exec_cmd("cliphist list | walker --dmenu | cliphist decode | wl-copy"))
 
 -- Move focus with mainMod + arrow keys
 hl.bind(mainMod .. " + h", hl.dsp.focus({ direction = "l" }))
