40ac02c29ab2e353a24c9024a8730f48cc095eb2
Sean Zhong <sean.zhong98@gmail.com>
2026-08-04 23:53:10 +0200

    Fix: Change openrgb config

--- modules/nixos/rgb.nix
+++ modules/nixos/rgb.nix
@@ -6,11 +6,9 @@
     package = pkgs.openrgb;
     motherboard = "amd";
     server = {
-            port = 6742;
-        };
+      port = 6742;
     };
+  };
 
-  boot.kernelModules = [ "i2c-dev" "i2c-piix4" ];
   boot.kernelParams = [ "acpi_enforce_resources=lax" ];
-  services.udev.extraRules =  builtins.readFile ../../resources/udev/60-openrgb.rules;
 }
